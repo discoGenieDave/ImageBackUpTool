e67611d14bdb73ed14ab874fdf7c7b3aa041c983
David Clabaugh <127340045+discoGenieDave@users.noreply.github.com>
2023-03-27 21:46:27 -0700

    Updated log files
    Added JSON dump of Backed up files and a printout of empty directories in the search

--- textureBackUpTool.py
+++ textureBackUpTool.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import argparse
+import json
 
 
 class ImageBackup:
@@ -8,6 +9,8 @@ class ImageBackup:
         self.root_dir = os.getcwd()
         self.name_string = name_string
         self.image_types = image_types
+        self.backup_files = []
+        self.no_image_dirs = []
 
     def backup_images(self):
         for dirpath, dirnames, filenames in os.walk(self.root_dir):
@@ -24,18 +27,29 @@ class ImageBackup:
                     src_path = os.path.join(dirpath, filename)
                     backup_path = os.path.join(bak_dir, filename)
                     shutil.copy2(src_path, backup_path)
-                    print(f"Backed up {filename} to {backup_path}")
+                    self.backup_files.append(filename)
+                    print(f"Backed up {filename}")
+            if not self.backup_files:
+                self.no_image_dirs.append(dirpath)
+                
+
+    def write_report(self):
+        report = {
+            'backup_files': self.backup_files,
+            'no_image_dirs': self.no_image_dirs
+        }
+        with open('backup_report.json', 'w') as f:
+            json.dump(report, f, indent=4)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Backup image files that contain a given string in their names.')
     parser.add_argument('-n', '--name', dest='name_string', default='',
-                        help='String that should be present in the filenames of the image files to be backed up ('
-                             'default: all image files)')
+                        help='String that should be present in the filenames of the image files to be backed up (default: all image files)')
     parser.add_argument('-t', '--types', dest='image_types', nargs='+', default=['.jpg', '.jpeg', '.png', '.gif'],
-                        help='List of file extensions for the image files to be backed up (default: .jpg, .jpeg, '
-                             '.png, .gif)')
+                        help='List of file extensions for the image files to be backed up (default: .jpg, .jpeg, .png, .gif)')
     args = parser.parse_args()
 
     backup = ImageBackup(args.name_string, args.image_types)
     backup.backup_images()
+    backup.write_report()
