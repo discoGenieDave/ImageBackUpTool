f5493bebfbdd35d88cc6ac43e9ed040c5afcd44e
David Clabaugh <127340045+discoGenieDave@users.noreply.github.com>
2023-03-27 22:55:20 -0700

    Update textureBackUpTool.py
    typo

--- textureBackUpTool.py
+++ textureBackUpTool.py
@@ -10,7 +10,7 @@ import json
 # 1 Place script in the root directory that needs to be searched.
 # 2. Make sure Python is installed. 
 # 3. Open Command Prompt or Terminal 
-# 4. type: python texctureBackUpTool.py -n name -t filetype 
+# 4. type: python textureBackUpTool.py -n name -t filetype 
 # 5. Use the name of the file you are looking for and the type of image 
 # 6. A back up dir called bak will be created next to each instance found. 
 ##########################################################################
