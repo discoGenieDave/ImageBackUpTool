20f6c959abdda3bd34b7231be23d532941739ae3
David Clabaugh <127340045+discoGenieDave@users.noreply.github.com>
2023-03-27 22:31:15 -0700

    Update textureBackUpTool.py
    Added Instructions in the header

--- textureBackUpTool.py
+++ textureBackUpTool.py
@@ -4,6 +4,20 @@ import argparse
 import json
 
 
+#####################################################################
+# Usage:
+#
+# 1 Place script in the root directory that needs to be searched.
+# 2. Make sure Python is installed. 
+# 3. Open Command Prompt or Terminal 
+# 4. type: python texctureBackUpTool.py -n name -t filetype 
+# 5. Use the name of the file you are looking for and the type of image 
+# 6. A back up dir called bak will be created next to each instance found. 
+##########################################################################
+
+
+
+
 class ImageBackup:
     def __init__(self, name_string='', image_types=['.jpg', '.jpeg', '.png', '.gif']):
         self.root_dir = os.getcwd()
